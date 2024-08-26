441fb38bfc7f9685d2dce5291f51c58b65351209
Gustavo Viana <gustaolive1234@gmail.com>
2024-08-26 13:47:35 -0300

    Adiciona arquivo para criar banco de dados

---
+++ testes/Criar Database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE login;
+USE login;
+CREATE TABLE `login`.`usuario` (
+  `usuario_id` INT NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(200) NOT NULL,
+  `senha` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`usuario_id`));
