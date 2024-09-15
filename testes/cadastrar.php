bb238402ceafc4a7f4f0e99cf41c9ec1a9f900ba
Gustavo <gustaolive1234@gmail.com>
2024-09-15 09:13:28 -0300

    Corrige bug com email na página de cadastro

--- testes/cadastrar.php
+++ testes/cadastrar.php
@@ -2,18 +2,22 @@
 session_start();
 include('conexao.php');
 
+// Função para validar e-mail
+function valida_email($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+
 // Verifica se campos de textos estão vazios 
 if (empty($_POST['usuario']) || empty($_POST['senha']) || empty($_POST['email'])) {
     header('location: cadastro.php');
     exit();
-} elseif (valida_email($_POST['email']) == false) {
-    $_SESSION["emailInvalido"] = true;
-    header('location: cadastro.php');
-    exit(); 
 }
 
-function valida_email($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
+// Verifica se o e-mail é válido
+if (valida_email($_POST['email']) === false) {
+    $_SESSION["emailInvalido"] = true;
+    header('location: cadastro.php');
+    exit();
 }
 
 // Captura os dados do campo de texto
@@ -21,33 +25,33 @@ $email = mysqli_real_escape_string($conexao, $_POST['email']);
 $usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
 
-// Cria query para mandar no SQL
+// Verifica se o usuário já está cadastrado
 $queryTeste = "SELECT usuario_id, usuario FROM usuario WHERE usuario = '{$usuario}'";
-
-// Executa a query
 $result = mysqli_query($conexao, $queryTeste);
-
-// Retorna a quantidade de linhas retornadas
 $row = mysqli_num_rows($result);
 
 if ($row >= 1) {
     $_SESSION["ja_cadastro"] = true;
     header('location: cadastro.php');
     exit();
-} elseif (isset($_SESSION["emailInvalido"]) && $_SESSION["emailInvalido"] === true) {
-    header('location: cadastro.php');
-    exit();
-} else {
-    // Cria query para mandar no SQL
-    $query = "INSERT INTO usuario (email, usuario, senha) VALUES ('{$email}', '{$usuario}', MD5('{$senha}'));";
-
-    // Executa a query
-    if (mysqli_query($conexao, $query)) {
-        $_SESSION['cadastrado'] = true;
-    } else {
-        echo "Erro ao cadastrar: " . mysqli_error($conexao);
-    }
-    header('Location: cadastro.php');
-    exit();
 }
+
+// Se o e-mail foi validado e não há usuário duplicado
+// Limpa a variável de sessão emailInvalido
+unset($_SESSION["emailInvalido"]);
+
+// Cria query para mandar no SQL
+$query = "INSERT INTO usuario (email, usuario, senha) 
+VALUES ('{$email}', '{$usuario}', MD5('{$senha}'));";
+
+// Executa a query
+if (mysqli_query($conexao, $query)) {
+    $_SESSION['cadastrado'] = true;
+} else {
+    echo "Erro ao cadastrar: " . mysqli_error($conexao);
+}
+// Redireciona para a página de cadastro
+header('Location: cadastro.php');
+exit();
+
 ?>
