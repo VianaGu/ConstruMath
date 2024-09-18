bf20644e094dc1bc54abcc61bc4d1ec53ed89bab
Gustavo <gustaolive1234@gmail.com>
2024-09-17 21:15:35 -0300

    Altera maneira da criação do banco de dados

--- paginas/CriarDatabase.sql
+++ paginas/CriarDatabase.sql
@@ -1,8 +1,16 @@
-CREATE DATABASE login;
+-- Criação do banco de dados
+CREATE DATABASE login
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_bin;
+
+-- Seleciona o banco de dados
 USE login;
-CREATE TABLE `login`.`usuario` (
-  `usuario_id` INT NOT NULL AUTO_INCREMENT,
-  `e-mail` VARCHAR(255) NOT NULL,
-  `usuario` VARCHAR(200) NOT NULL,
-  `senha` VARCHAR(32) NOT NULL,
-  PRIMARY KEY (`usuario_id`));
+
+-- Criação da tabela com collation case-sensitive para a coluna 'usuario'
+CREATE TABLE usuario (
+  usuario_id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL, 
+  usuario VARCHAR(255) COLLATE utf8mb4_bin NOT NULL, -- Definido COLLATE utf8mb4_bin para case-sensitive
+  senha VARCHAR(32) NOT NULL,
+  PRIMARY KEY (usuario_id)
+);
